fd6d6a2d247a2463b93dcde8377abf508cc374ed
Jesuloba <jesulobajohn@gmail.com>
2022-07-03 19:23:35 +0100

    first commit: setting up

---
+++ styled.config.ts
@@ -0,0 +1,13 @@
+interface Theme {
+	[key: string]: string;
+}
+
+export const theme: Theme = {
+	black: "#000000",
+	smoky: "#101010",
+	sienna: "#d87d4a",
+	light_salmon: "#fbaf85",
+	cultured: "#f1f1f1",
+	grey_white: "#fafafa",
+	white: "#ffffff",
+};
