2c729caf334580af386a0377d9e976deb2714a02
Jesuloba <jesulobajohn@gmail.com>
2022-07-04 01:47:05 +0100

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
+		colors: {
+			black: "#000000",
+			smoky: "#101010",
+			sienna: "#d87d4a",
+			light_salmon: "#fbaf85",
+			cultured: "#f1f1f1",
+			grey_white: "#fafafa",
+			white: "#ffffff",
+		},
 		extend: {},
 	},
 	plugins: [],
