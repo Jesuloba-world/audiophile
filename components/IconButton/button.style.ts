fd5289fd9e08a2f3f666063d50956ea3aed26343
Jesuloba <jesulobajohn@gmail.com>
2022-07-05 12:33:09 +0100

    iconbuttons hover effect

--- components/IconButton/button.style.ts
+++ components/IconButton/button.style.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Container = styled.button`
 	color: ${({ theme }) => theme.white};
 	font-size: 23px;
+
+	&:hover {
+		color: ${({ theme }) => theme.sienna};
+	}
 `;
