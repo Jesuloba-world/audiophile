b1b19adcf36b27b174018cde381652943b7d6672
Jesuloba <jesulobajohn@gmail.com>
2022-07-05 14:40:20 +0100

    achieved 100% test coverage and starting footer

---
+++ containers/layout/main/Footer/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Container } from "./footer.style";
+
+interface footerProps {}
+
+export const Footer: FC<footerProps> = () => {
+	return <Container>This is the footer</Container>;
+};
--- containers/layout/main/mainlayout.test.tsx
+++ containers/layout/main/mainlayout.test.tsx
@@ -2,11 +2,27 @@ import { MainLayout } from "./index";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
+jest.mock("next/head", () => {
+	return {
+		__esModule: true,
+		default: ({ children }: { children: Array<React.ReactElement> }) => {
+			return <>{children}</>;
+		},
+	};
+});
+
 describe("Layout component", () => {
 	it("Renders layout component", () => {
-		render(<MainLayout>Test</MainLayout>);
-		screen.getByText(/test/i);
+		const tree = render(<MainLayout>Test</MainLayout>);
+		expect(tree).toMatchSnapshot();
 	});
+
+	it("Contains a title prop", () => {
+		const testTitle = "Test Title";
+		render(<MainLayout title={testTitle}>Test</MainLayout>);
+		expect(document.title).toBe(testTitle);
+	});
+
 	it("Renders Header", () => {
 		render(<MainLayout>Test</MainLayout>);
 		screen.getByText(/home/i);
