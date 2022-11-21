78945256bb035388f4072772f15d2812c8090b6d
Jesuloba <jesulobajohn@gmail.com>
2022-11-21 03:07:13 +0100

    some changes

--- components/Modal/CartModal/index.tsx
+++ components/Modal/CartModal/index.tsx
@@ -1,7 +1,21 @@
 import { Container } from "./styles";
 import { GenButton } from "components";
+import { useQuery } from "@apollo/client";
+import { MyCartDocument } from "src/graphql/generated";
+import { useMe } from "hooks";
+import { useEffect } from "react";
 
 export const CartModal = () => {
+	const { loading, loggedIn } = useMe();
+
+	const cart = useQuery(MyCartDocument);
+
+	useEffect(() => {
+		!loading && loggedIn && cart.refetch();
+	}, [loading, loggedIn, cart]);
+
+	!loading && console.log(cart.data);
+
 	return (
 		<Container>
 			<h1>The Cart</h1>
