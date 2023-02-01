00cce1936d65f6397eed8824239225f4e280a805
jesuloba-world <jesulobajohn@gmail.com>
2023-02-01 19:17:54 +0100

    removed redundant auth stuff

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,22 +12,6 @@ import {
 } from "src/graphql/generated";
 import jwtDecode from "jwt-decode";
 
-const formatUnixDate = (unixDate: number) => {
-	const date = new Date(unixDate * 1000);
-	// Hours part from the timestamp
-	const hours = date.getHours();
-	// Minutes part from the timestamp
-	const minutes = "0" + date.getMinutes();
-	// Seconds part from the timestamp
-	const seconds = "0" + date.getSeconds();
-
-	// Will display time in 10:30:23 format
-	const formattedTime =
-		hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-
-	return formattedTime;
-};
-
 export const authOptions: AuthOptions = {
 	providers: [
 		CredentialsProvider({
