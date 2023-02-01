b50c5f9d29e3ad84a6bd588d2e7300dfd56c92d7
jesuloba-world <jesulobajohn@gmail.com>
2023-02-01 19:13:13 +0100

    fixed refresh error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,6 +12,22 @@ import {
 } from "src/graphql/generated";
 import jwtDecode from "jwt-decode";
 
+const formatUnixDate = (unixDate: number) => {
+	const date = new Date(unixDate * 1000);
+	// Hours part from the timestamp
+	const hours = date.getHours();
+	// Minutes part from the timestamp
+	const minutes = "0" + date.getMinutes();
+	// Seconds part from the timestamp
+	const seconds = "0" + date.getSeconds();
+
+	// Will display time in 10:30:23 format
+	const formattedTime =
+		hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+
+	return formattedTime;
+};
+
 export const authOptions: AuthOptions = {
 	providers: [
 		CredentialsProvider({
@@ -174,12 +190,14 @@ export const authOptions: AuthOptions = {
 
 					return {
 						...token, // Keep the previous token properties
-						access_token: tokens.token,
+						accessToken: tokens.token,
 						tokenExpiresIn: decoded.exp,
-						refresh_token: tokens.refreshToken,
-					};
-				} catch (error) {
-					// console.error("Error refreshing access token", error);
+						refreshToken: tokens.refreshToken,
+					} as Awaitable<JWT>;
+				} catch (error: any) {
+					console.error(
+						error.errors[Object.keys(error.errors)[0]][0].message
+					);
 					return {
 						...token,
 						error: "RefreshAccessTokenError",
